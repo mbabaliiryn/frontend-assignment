fb87612768790ad38947c7ea0267cf32eb2338bd
mbabaliiryn <mbabaliiryn@gmail.com>
2021-06-25 19:54:39 +0300

    create small React movie app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,74 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 
-const App = () => (
-	<img src={logo} alt="Timescale" />
-)
+import Movie from "./Movie";
+import logo from "./timescale.png";
 
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7315ec59ea2264da1fa4f4eb8d647853&page=1";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=7315ec59ea2264da1fa4f4eb8d647853&query=";
+
+function App() {
+	const [movies, setMovies] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
+
+	useEffect(() => {
+		getMovies(FEATURED_API);
+	}, []);
+
+	const getMovies = (API) => {
+		fetch(API)
+		.then((res) => res.json())
+		.then((data) => {
+			setMovies(data.results);
+		})
+	}
+
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
+
+		if(setSearchTerm){
+		getMovies(SEARCH_API + searchTerm);
+		setSearchTerm("");
+	}
+	};
+
+	const handleOnChange = (e) => {
+		console.log(e.target.value);
+		setSearchTerm(e.target.value);
+	}
+
+	return (
+		<>
+			<header>
+
+				<div className="logo">
+					<img className="logo-image" src={logo} alt="title"/>
+					<span>Timescale</span>
+				</div>
+			
+				<form onSubmit={handleOnSubmit} className="icon">
+				<input className="search" 
+				
+				type="search" placeholder="search for a movie"
+				value={searchTerm}
+				onChange={e=>handleOnChange(e)}
+				
+
+				/>
+				<i class="fas fa-search"></i>
+				
+				</form>
+			</header>
+			<div className="movie-heading"><p>Most Recent Movies</p></div>
+		
+		<div className="movie-container">
+			
+			{movies.length > 0 && movies.map((movie) => 
+			<Movie key={movie.id} {...movie} />)}
+				
+		</div>
+		</>
+	)
+}
 export default App;
