fb81268fcdb8e12c64d46c7ddfdc14f8e82deabd
mbabaliiryn <mbabaliiryn@gmail.com>
2021-06-25 23:26:32 +0300

    add vote coverage

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,11 +7,13 @@ const Movie = ({ title, poster_path, overview, vote_average}) => (
         <img src={IMG_API + poster_path} alt={title}/>
         <div className="movie-info">
             <h3>{title}</h3>
+            <p className="vote">{vote_average}</p>
             
         </div>
         <div className="movie-over">
             <h2>overview:</h2>
             <p>{overview}</p>
+           
 
         </div>
     </div>
